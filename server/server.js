972d2e34ebbc099dc909b5f082f32a271e77d634
prashant <prashantburde84@gmail.com>
2025-11-04 18:49:05 +0530

    feat(server): add get route code to indicate server running..

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
+app.get((req,res)=>{
+  console.log("Server is running...")
+})
+
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
